2116b324df956654fd80d109bfe3cedb0e752647
suven <pf_qq@163.com>
2025-11-17 21:02:09 +0800

    mybatis 多租户标签注解忽略实现

--- future-utils/src/main/java/com/suven/framework/core/mybatis/TenantIgnoreHelper.java
+++ future-utils/src/main/java/com/suven/framework/core/mybatis/TenantIgnoreHelper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -12,13 +13,20 @@ import java.util.Set;
 @Component
 public class TenantIgnoreHelper {
     
-    @Autowired
+    @Autowired(required = false)
     private IgnoreTenantLineHandler ignoreTenantLineHandler;
-    
+
+    public boolean isIgnoreNull() {
+        return this.ignoreTenantLineHandler == null;
+    }
+
     /**
      * 检查实体类是否忽略租户
      */
     public boolean isIgnoreTenant(Class<?> entityClass) {
+        if (isIgnoreNull()){
+            return false;
+        }
         return ignoreTenantLineHandler.isIgnoreByEntityClass(entityClass);
     }
     
@@ -26,6 +34,9 @@ public class TenantIgnoreHelper {
      * 检查表是否忽略租户
      */
     public boolean isIgnoreTenant(String tableName) {
+        if (isIgnoreNull()){
+            return false;
+        }
         return ignoreTenantLineHandler.isIgnoreByTableName(tableName);
     }
     
@@ -33,6 +44,9 @@ public class TenantIgnoreHelper {
      * 获取所有忽略租户的表
      */
     public Set<String> getIgnoreTenantTables() {
+        if (isIgnoreNull()){
+            return new HashSet<>();
+        }
         return ignoreTenantLineHandler.getAllIgnoreTables();
     }
 }
