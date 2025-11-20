d06928bb06921c666ecdbff945b71fb661a30d9c
suven <pf_qq@163.com>
2025-11-20 23:47:02 +0800

    redis 聚群优化

--- future-utils/src/main/java/com/suven/framework/core/redis/factory/RedisConnectionInCluster.java
+++ future-utils/src/main/java/com/suven/framework/core/redis/factory/RedisConnectionInCluster.java
@@ -2,9 +2,11 @@ package com.suven.framework.core.redis.factory;
 
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 然后，实现具体的连接策略类，包括单机模式、哨兵模式、集群模式和主从模式。每个策略类都实现了 RedisConnectionStrategy 接口，并根据对应的连接方式创建 RedisConnectionFactory 实例：
@@ -26,6 +28,9 @@ public class RedisConnectionInCluster extends AbstractRedisConnection implements
     @Override
     public RedisConnectionFactory createConnectionFactory() {
         RedisClusterConfiguration config = new RedisClusterConfiguration(nodes);
+        if (Objects.nonNull(redisConfig.getPassword())){
+            config.setPassword(RedisPassword.of(redisConfig.getPassword()));
+        }
         config.setMaxRedirects(maxRedirects);
         return new LettuceConnectionFactory(config);
     }
--- future-utils/src/main/java/com/suven/framework/core/redis/factory/RedisConnectionStrategyFactory.java
+++ future-utils/src/main/java/com/suven/framework/core/redis/factory/RedisConnectionStrategyFactory.java
@@ -1,12 +1,11 @@
 package com.suven.framework.core.redis.factory;
 
 
-import com.suven.framework.core.redis.RedisModelType;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RedisConnectionStrategyFactory extends AbstractRedisConnection implements RedisConnectionStrategy{
+public class RedisConnectionStrategyFactory extends AbstractRedisConnection {
 
 
 
@@ -15,54 +14,56 @@ public class RedisConnectionStrategyFactory extends AbstractRedisConnection impl
     }
 
     /**
-     *
-     * @return
+     * 根据配置的 Redis 连接模式创建对应的连接工厂
+     * 通过枚举 {@link RedisConnectionEnum} 来确定使用哪种连接策略
+     * 
+     * @return RedisConnectionFactory Redis 连接工厂实例
      */
     @Override
     public RedisConnectionFactory createConnectionFactory() {
-//        RedisConnectionEnum redisEnum = RedisConnectionEnum.getEnum(getRedisConfig().getClient().getModel());
-        RedisConnectionStrategy strategy = null;
-        RedisModelType redisModelType = getRedisModelType();
-        if (redisModelType.isStandalone()){
-            strategy = new RedisConnectionInStandalone(redisConfig);
-        }if (redisModelType.isMasterSlave()){
-            strategy = new RedisConnectionInMasterSlave(redisConfig);
-        }if (redisModelType.isSentinel()){
-            strategy = new RedisConnectionInSentinel(redisConfig);
-        }if (redisModelType.isCluster()){
-            strategy = new RedisConnectionInCluster(redisConfig);
-        }if (redisModelType.isClusterGroup()){
-            strategy = new RedisConnectionInCluster(redisConfig);
-        }
-        if (strategy == null){
-            strategy = new RedisConnectionInStandalone(redisConfig);
-        }
-        RedisConnectionFactory factory = strategy.createConnectionFactory();
-        return factory;
+        // 从配置中获取 Redis 连接模式（standalone/sentinel/masterSlave/cluster/clusterGroup）
+        String model = getRedisConfig().getClient().getModel();
+        RedisConnectionEnum redisEnum = RedisConnectionEnum.getEnum(model);
+        
+        // 根据枚举类型创建对应的连接策略实例
+        RedisConnectionStrategy strategy = createStrategyByEnum(redisEnum);
+        
+        // 创建并返回连接工厂
+        return strategy.createConnectionFactory();
+    }
 
-//        switch (redisEnum){
-//            case REDIS_STANDALONE_STRATEGY:{
-//                strategy = new RedisConnectionInStandalone(redisConfig);
-//                break;
-//            }
-//            case REDIS_SENTINEL_STRATEGY:{
-//                strategy = new RedisConnectionInSentinel(redisConfig);
-//                break;
-//            }
-//            case REDIS_MASTER_SLAVE_STRATEGY:{
-//                strategy = new RedisConnectionInMasterSlave(redisConfig);
-//                break;
-//            }
-//            case REDIS_CLUSTER_STRATEGY:
-//            case REDIS_CLUSTER_GROUP_STRATEGY:{
-//                strategy = new RedisConnectionInCluster(redisConfig);
-//                break;
-//            } default:{
-//                strategy = new RedisConnectionInStandalone(redisConfig);
-//            }
-//        }
-//        RedisConnectionFactory factory = strategy.createConnectionFactory();
-//        return factory;
+    /**
+     * 根据 Redis 连接枚举创建对应的连接策略实例
+     * 
+     * @param redisEnum Redis 连接枚举类型，如果为 null 则使用默认的单机模式
+     * @return RedisConnectionStrategy Redis 连接策略实例
+     */
+    private RedisConnectionStrategy createStrategyByEnum(RedisConnectionEnum redisEnum) {
+        if (redisEnum == null) {
+            // 如果枚举为 null，使用默认的单机模式
+            return new RedisConnectionInStandalone(redisConfig);
+        }
+
+        // 使用 switch 语句根据枚举类型创建对应的策略实例
+        switch (redisEnum) {
+            case REDIS_STANDALONE_STRATEGY:
+                return new RedisConnectionInStandalone(redisConfig);
+                
+            case REDIS_SENTINEL_STRATEGY:
+                return new RedisConnectionInSentinel(redisConfig);
+                
+            case REDIS_MASTER_SLAVE_STRATEGY:
+                return new RedisConnectionInMasterSlave(redisConfig);
+                
+            case REDIS_CLUSTER_STRATEGY:
+            case REDIS_CLUSTER_GROUP_STRATEGY:
+                // 集群模式和集群组模式都使用相同的集群连接策略
+                return new RedisConnectionInCluster(redisConfig);
+                
+            default:
+                // 未知模式，默认使用单机模式
+                return new RedisConnectionInStandalone(redisConfig);
+        }
     }
 
 
