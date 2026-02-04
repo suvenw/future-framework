a99f58c9e19bf65ed21be610d295323f92810467
suven <pf_qq@163.com>
2026-02-04 21:05:50 +0800

    全面升级 优化代码编写逻辑实现,使用代码更简洁可读

--- future-http-file/src/main/java/com/suven/framework/fileinter/entity/DataSourceModuleName.java
+++
@@ -1,5 +0,0 @@
-package com.suven.framework.fileinter.entity;
-
-public interface DataSourceModuleName{
-          public static final String module_name_file = "file";
-  }
--- future-http-file/src/main/java/com/suven/framework/fileinter/entity/FileBusinessCallbackWater.java
+++
@@ -1,9 +0,0 @@
-package com.suven.framework.fileinter.entity;
-
-import com.suven.framework.http.data.entity.BaseTenantEntity;
-
-public class FileBusinessCallbackWater extends BaseTenantEntity {
-    public static FileBusinessCallbackWater build(){
-        return new FileBusinessCallbackWater();
-    }
-}
